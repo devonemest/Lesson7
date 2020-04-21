7f519c3d0a831f6a7b600eee56bf6b69b848bd02
devonemest <48983182+devonemest@users.noreply.github.com>
2020-04-21 12:31:02 +0300

    test

---
+++ Main.py
@@ -0,0 +1,15 @@
+# Виды циклов:
+# for
+# while
+# Для си:
+# do
+# repeat
+
+
+zarplata = int(input())
+while zarplata > 10:
+    print("Пора работать")
+    break
+else:
+    print("Увольняюсь")
+
